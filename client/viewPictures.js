ec0adefcfb3ff9b7c7ab59bbaf6751e1e7e91836
Patrick Levy <levyp@onid.oregonstate.edu>
2015-11-27 11:07:03 -0600

    update

--- client/viewPictures.js
+++ client/viewPictures.js
@@ -3,83 +3,55 @@ Meteor.startup(function() {
 });
 
 var randPhoto;
-var latitude = 44.9778;
-var longitude = -93.2650;
-
+var latitude;
+var longitude;
 Template.viewPictures.helpers( {
 	'photo': function () {
+		//Set current photo to be displayed
 		var numPhotos = Photos.find({}).count();
 		var randPhotoIndex = _.random(0, numPhotos - 1);
 		randPhoto = 'photo' + randPhotoIndex;
 		Session.set('currentPhoto', localStorage.getItem(randPhoto));
 
+		//Get geolocation of current photo for google map
 		var curPhoto = Photos.find({"photoName": randPhoto}).fetch();
-		console.log("curPhoto", curPhoto);
-		console.log("curPhoto[0].photolocation", curPhoto[0].photolocation);
 		latitude = curPhoto[0].photolocation.latitude;
 		longitude = curPhoto[0].photolocation.longitude;
 		Session.set("latitude", latitude);
 		Session.set("longitude", longitude);
 		
-
 		return Session.get('currentPhoto');
-	},
-
-	
+	},	
 });
 
 Template.viewPictures.events( {
 	'click .shake': function () {
-		// var numPhotos = Photos.find({}).count();
-		// var randPhotoIndex = _.random(0, numPhotos - 1);
-		// randPhoto = 'photo' + randPhotoIndex;
-		// //Session.set('currentPhoto', Session.get(randPhoto));
-		// var curPhoto = Photos.find({"photoName": randPhoto}).fetch();
-		// latitude = curPhoto[0].photolocation.latitude;
-		// longitude = curPhoto[0].photolocation.longitude;
-		// Session.set("latitude", latitude);
-		// Session.set("longitude", longitude);
-		
-		// Session.set('currentPhoto', localStorage.getItem(randPhoto));
-		location.reload();
-		
+		location.reload();	
 	}
 });
 
+Template.map.helpers({  
+  mapOptions: function() {
+  	//var curPhoto = Session.get('currentPhoto');
+    if (GoogleMaps.loaded()) {
+
+      return {
+        center: new google.maps.LatLng(Session.get("latitude"), Session.get("longitude")),
+        zoom: 15
+      };
+    }
+  }
+});
+
+Template.map.onCreated(function() {  
+  GoogleMaps.ready('map', function(map) {
+
+     var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
+  	});
+
+  });
+});
 
 
-
-
-	Template.map.helpers({  
-	  mapOptions: function() {
-	  	//var curPhoto = Session.get('currentPhoto');
-	    if (GoogleMaps.loaded()) {
-
-	      return {
-	        center: new google.maps.LatLng(Session.get("latitude"), Session.get("longitude")),
-	        zoom: 15
-	      };
-	    }
-	  }
-	});
-
-	Template.map.onCreated(function() {  
-	  GoogleMaps.ready('map', function(map) {
-	     console.log("I'm ready!");
-
-	     var marker = new google.maps.Marker({
-	      position: map.options.center,
-	      map: map.instance
-
-	  	});
-
-	  });
-	});
-
-
-
-// Tracker.autorun(function () {
-//   var curPhoto = Session.get('currentPhoto');
-//   console.log('Autorun is auto-running!');
-//   console.log(count);
-// });
